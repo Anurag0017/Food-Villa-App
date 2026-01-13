e4d94b814093772c8e29891b595d5a689dc35096
Anurag Chaurasia <anuragchaurasia8090@gmail.com>
2026-01-13 12:21:22 +0530

    Update image CDN URL and API endpoint

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,8 +1,7 @@
- export const IMG_CDN_URL="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
  
  export const shimmer_card_unit = 20;
- export const swiggy_api_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+ export const swiggy_api_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.8310607&page_type=DESKTOP_WEB_LISTING";
  
  export const restaurantList = [
     {
